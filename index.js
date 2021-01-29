f5cbf3c65a27064ae6ecaa741b3cbbe313ab1c36
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-01-29 14:13:00 -0500

    Initial commit of files

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquir
+
+
+
+
+
+
+
+
+
+
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
